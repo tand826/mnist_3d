91193db8e7a4fc34c10bf3ff4e22d992c122ed2b
tand826 <takumi.ando826@gmail.com>
2019-06-23 17:38:25 +0900

    update

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # mnist_3d
 - Plot mnist data in 3d space.
 
+# Example
+![2D plot](images/2d.png "2d")
+
+![3D plot](images/3d.png "3d")
+
 # Requirements
 python version >= 3.6
 ```
@@ -8,15 +13,25 @@ pip install -r requirements.txt
 ```
 
 # Run
-### Save mnist data as png
+### (1) Save mnist data as png
 ```
 python mnist_to_png.py
 ```
 
-### Plot mnist data
+### (2) Plot mnist data
 ```
 python mnist_to_3dplot.py
 ```
 
 # What does it do?
-- T-SNE + TruncatedSVD
+- Truncated SVD
+- T-SNE
+
+# Arguments
+| Arguments | Default | Example |
+|:-----------|:------------|:------------|
+| sample_number | 100 | 1000 |
+| phase | train | test |
+| svd_dimension | 100 | 500 |
+| out_dimension | 2 | 3 |
+| sampled_count | False | (nothing) |
--- mnist_to_3dplot.py
+++ mnist_to_3dplot.py
@@ -1,4 +1,6 @@
 import argparse
+import random
+
 from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,40 +12,54 @@ from mpl_toolkits.mplot3d import Axes3D
 def load_imgs(root, phase, count):
     imgs = []
     labels = []
-    paths = list(root.glob(f"{phase}/*/*.png"))[:count]
+    paths_all = list(root.glob(f"{phase}/*/*.png"))
+    paths = random.sample(paths_all, count)
     for path in paths:
         imgs.append(plt.imread(str(path)).reshape(1, 784))
         labels.append(np.array(int(path.parent.stem)).reshape(1,))
-    return imgs, labels
 
-
-def reduce_dim(imgs, labels, dimension):
     imgs_reshaped = np.concatenate(imgs, axis=0)
     labels_reshaped = np.concatenate(labels, axis=0)
-
-    imgs_svd = TruncatedSVD(n_components=dimension).fit_transform(imgs_reshaped)
-    imgs_reduced = TSNE(n_components=3,  verbose=1).fit_transform(imgs_svd)
-
-    return imgs_reduced, labels_reshaped
+    return imgs_reshaped, labels_reshaped
 
 
-def plot(imgs, labels):
+def reduce_dim(imgs, svd_dim, out_dim):
+    imgs_svd = TruncatedSVD(n_components=svd_dim).fit_transform(imgs)
+    imgs_reduced = TSNE(n_components=out_dim,  verbose=1).fit_transform(imgs_svd)
+    return imgs_reduced
+
+
+def show_count(labels):
+    for i in range(10):
+        print(f"{i} : {np.sum(labels == i)}")
+
+
+def plot(imgs, labels, out_dim):
     fig = plt.figure(dpi=200)
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter3D(imgs[:, 0], imgs[:, 1], imgs[:, 2], c=labels)
+    if out_dim == 3:
+        ax = fig.add_subplot(111, projection='3d')
+        scatter = ax.scatter3D(imgs[:, 0], imgs[:, 1], imgs[:, 2], c=labels)
+    else:
+        ax = fig.add_subplot(111)
+        scatter = ax.scatter(imgs[:, 0], imgs[:, 1], c=labels)
+    plt.colorbar(scatter)
     plt.show()
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("count", type=int)
-    parser.add_argument("phase", choices=["train", "test"])
-    parser.add_argument("dimension", type=int)
+    parser.add_argument("-sn", "--sample_number", type=int, default=100)
+    parser.add_argument("-p", "--phase", choices=["train", "test"], default="train")
+    parser.add_argument("-sd", "--svd_dimension", type=int, default=100)
+    parser.add_argument("-od", "--out_dimension", choices=[2, 3], type=int, default=3)
+    parser.add_argument("-sc", "--sampled_count", action='store_true')
     args = parser.parse_args()
 
-    imgs, labels = load_imgs(Path(), args.phase, args.count)
-    imgs_reduced, labels = reduce_dim(imgs, labels, args.dimension)
-    plot(imgs_reduced, labels)
+    imgs, labels = load_imgs(Path(), args.phase, args.sample_number)
+    imgs_reduced = reduce_dim(imgs, args.svd_dimension, args.out_dimension)
+    if args.sampled_count:
+        show_count(labels)
+    plot(imgs_reduced, labels, args.out_dimension)
 
 
 if __name__ == '__main__':
