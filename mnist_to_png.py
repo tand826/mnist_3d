2200737c9839c024d12657c757449f1fe0a65376
tand826 <takumi.ando826@gmail.com>
2019-06-23 18:35:36 +0900

    update

--- mnist_to_png.py
+++ mnist_to_png.py
@@ -1,31 +1,24 @@
-import os
-from PIL import Image
-import chainer
-
-
-def save(data, index, num, phase):
-    img = Image.new("L", (28, 28))
-    pix = img.load()
-    for i in range(28):
-        for j in range(28):
-            pix[i, j] = int(data[i+j*28]*256)
-    img2 = img.resize((28, 28))
-    filename = phase + "/" + str(num) + "/" + "{0:04d}".format(index) + ".png"
-    img2.save(filename)
-    print(filename)
+from torchvision import datasets, transforms
+from pathlib import Path
 
 
 def main():
-    train, test = chainer.datasets.get_mnist()
-    for phase, data in {"train": train, "test": test}.items():
-        if os.path.isdir(phase) is False:
-            os.mkdir(phase)
-        for i in range(10):
-            dirname = phase + "/" + str(i)
-            if os.path.isdir(dirname) is False:
-                os.mkdir(dirname)
-        for i in range(len(data)):
-            save(data[i][0], i, data[i][1], phase)
+    to_pil_image = transforms.ToPILImage()
+    for phase in ["train", "test"]:
+        base = Path(phase)
+        mkdirs(base)
+        is_train = phase == "train"
+        mnist = datasets.MNIST(".", train=is_train, download=True)
+        for idx, (img, label) in enumerate(zip(mnist.data, mnist.targets)):
+            name = base/str(label.item())/f"{idx:04}.png"
+            to_pil_image(img).save(str(name))
+
+
+def mkdirs(base):
+    if not base.exists():
+        base.mkdir(exist_ok=True)
+    for i in range(10):
+        (base/str(i)).mkdir(exist_ok=True)
 
 
 if __name__ == '__main__':
